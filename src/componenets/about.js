6a72b8c829e1ce354ca2605f848a406b847b3e2a
thejas <thejaskala308@gmail.com>
2022-06-28 19:46:35 +0530

    secosnd commit

--- src/componenets/about.js
+++ src/componenets/about.js
@@ -5,7 +5,7 @@ const about = () => {
         <div>
             <div className='ui raised very padded text container segment' style={{marginTop:'45px'}}>
             <h1>About Us</h1>
-           <p>hello my name is Thejas Hari i developing this website with my potato Pc even it will hang just to compile this code but i never give i will keep trying thank you!</p>
+           <p>hello my name is Thejas i developing this website with my potato Pc even it will hang just to compile this code but i never give i will keep trying thank you!</p>
        </div></div>
     )
 }
